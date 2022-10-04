4eb858dc0856ff908306d82d031d7354eb4655d8
Hydrogen <3038094028@qq.com>
2022-10-05 03:00:13 +0800

    sw

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,36 @@ import compileClass from '@unocss/transformer-compile-class'
 
 import vue from '@vitejs/plugin-vue'
 import jsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [unocss({
     presets: [presetUno(), presetIcons({})],
     include: ['src/**/*.tsx', 'src/**/*.vue', 'src/**/*.jsx'],
     transformers: [directives(), compileClass()]
-  }), vue(), jsx()],
+  }), vue(), jsx(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,ts,tsx,woff2,woff,ttf,otf}'],
+      runtimeCaching: [{
+        urlPattern: /^.*(\.md|\.markdown)$/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'articles-cache',
+          expiration: {
+            maxEntries: 300,
+            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }],
+    }
+  })],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
