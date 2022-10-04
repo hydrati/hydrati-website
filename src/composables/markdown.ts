9bdc9587ca6affbf5ae0129258e1e87b41ffd038
Hydrogen <3038094028@qq.com>
2022-10-05 00:01:21 +0800

    chore: sync

--- src/composables/markdown.ts
+++ src/composables/markdown.ts
@@ -1,18 +1,38 @@
-import MarkdownIt from 'markdown-it'
-import emoji from 'markdown-it-emoji'
-import addr from 'markdown-it-abbr'
-import footnote from 'markdown-it-footnote'
-import sub from 'markdown-it-sub'
-import sup from 'markdown-it-sup'
-import matter from 'gray-matter-browser'
-import hljs from 'markdown-it-highlightjs'
+import type MarkdownIt from 'markdown-it'
+// import emoji from 'markdown-it-emoji'
+// import abbr from 'markdown-it-abbr'
+// import footnote from 'markdown-it-footnote'
+// import sub from 'markdown-it-sub'
+// import sup from 'markdown-it-sup'
+import type matter from 'gray-matter-browser'
+// import hljs from 'markdown-it-highlightjs'
 
-const md = new MarkdownIt({
+const [
+  { default: MarkdownIt2 },
+  { default: emoji },
+  { default: abbr },
+  { default: footnote },
+  { default: sub },
+  { default: sup },
+  { default: matter2 },
+  { default: hljs },
+] = await Promise.all([
+  import('markdown-it'),
+  import('markdown-it-emoji'),
+  import('markdown-it-abbr'),
+  import('markdown-it-footnote'),
+  import('markdown-it-sub'),
+  import('markdown-it-sup'),
+  import('gray-matter-browser'),
+  import('markdown-it-highlightjs'),
+])
+
+const md = new MarkdownIt2({
   html: true,
   linkify: false,
 })
 
-md.use(emoji).use(addr).use(footnote).use(sub).use(sup).use(hljs)
+md.use(emoji).use(abbr).use(footnote).use(sub).use(sup).use(hljs)
 
 export function useMarkdownIt(): MarkdownIt {
   return md
@@ -21,7 +41,7 @@ export function useMarkdownIt(): MarkdownIt {
 export function useMarkdown(
   markdown: string
 ): [string, matter.GrayMatterFile<string>] {
-  const file = matter(markdown)
+  const file = matter2(markdown)
   const md = useMarkdownIt()
   return [md.render(file.content), file]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,8 @@ export default defineConfig({
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp'
     }
+  },
+  build: {
+    target: ['esnext']
   }
 })
